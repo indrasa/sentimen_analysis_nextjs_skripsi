588fd901c7853589b751b07ede45f539a69f6975
Indra Saputra Ahmadi <ahmadisaputraindra@gmail.com>
2026-01-04 22:36:57 +0800

    shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -59,6 +59,11 @@ export default function Home() {
             Documentation
           </a>
         </div>
+         */}
+         kata kata
+         form input dan tombol
+        <h1>Halo</h1>
+
       </main>
     </div>
   );
