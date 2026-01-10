e1a79fbb33c5fb36ef29e867d8e7ae622bb2ee2b
Indra Saputra Ahmadi <ahmadisaputraindra@gmail.com>
2026-01-10 20:00:44 +0800

    Add sentiment analysis components and input validation for YouTube URLs

---
+++ app/input_url_youtube.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+
+const isValidYoutubeURL = (url: string): boolean => {
+    const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)\/(watch\?v=[-\w]{11}|youtu\.be\/[-\w]{11}|shorts\/[-\w]{11})/;
+    return youtubeRegex.test(url);
+}
+
+export default function InputURLYoutube(){
+    const [URL, setURL] = useState();
+    
+    const handlePerubahanURL = (event) => {
+        setURL(event.target.value);
+    }
+
+    const handleUrl = () => {
+        if (!URL || !isValidYoutubeURL(URL)) {
+            alert('Mohon masukkan link YouTube yang valid');
+            //todo: ganti alert dengan modal yang bagus
+            return;
+        }
+        alert(`LINK VALID: ${URL}`);
+    }
+
+    return <>
+        <Input onChange={handlePerubahanURL} />
+        <Button onClick={handleUrl} variant="default">Analisis Komentar</Button>
+    </>
+}
---
+++ app/sentimen.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Sentimen(){
+    return <>
+        di sini halaman sentimen wokh
+    </>
+}
