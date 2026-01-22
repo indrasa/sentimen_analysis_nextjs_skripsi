af487ec5c531f91f58ee29e38c7c4d34a7cf8694
Indra Saputra Ahmadi <ahmadisaputraindra@gmail.com>
2026-01-22 18:45:38 +0800

    checkpoint ke pc

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import AllInsightSentimen from '@/components/all_insight_sentimen';
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-8 md:px-32 bg-white dark:bg-black sm:items-start">
         <div>
           <h1 className="text-5xl font-black text-center mb-4">Analisis Sentimen Komentar Youtube</h1>
           <p className="text-center">Masukkan URL video Youtube untuk mengetahui sentimen komentar secara otomatis dengan teknologi AI canggih.</p>
-          <div className="flex space-x-3 mt-7">
+          <div className="flex flex-col gap-2 md:flex-row space-x-3 mt-7">
             <InputURLYoutube />
           </div>
          <AllInsightSentimen />
--- components/all_insight_sentimen.tsx
+++ components/all_insight_sentimen.tsx
@@ -42,11 +42,27 @@ export default function AllInsightSentimen() {
             {sentimenResults.map(item => (
                 <h1 key={item.text}>{item.text}</h1>
             ))} */}
-            <div className="my-9 grid grid-cols-2 md:grid-cols-5 col-span-1 gap-6">
+            <div className="my-9 grid grid-cols-1 md:grid-cols-5 col-span-1 gap-6">
                 <AllKartuSentimen />
             </div>
+{// next todo: perbaiki tampilan chart dan insight agar fit kiri kanan
+// pakai contoh ini
+                // ...existing code...
+                // <div className="flex flex-col md:flex-row mx-auto gap-5 justify-center w-full">
+                //     <div className="self-center md:self-stretch flex-1">
+                //         <ChartSentimen />
+                //     </div>
+                //     <div className='flex flex-col gap-3 flex-1'>
+                //         <h1 className="text-3xl font-bold text-center">Insight Cepat</h1>
+                //         <Card className='px-5 py-5'>insight 1</Card>
+                //         <Card className='px-5 py-5'>insight 1</Card>
+                //         <Card className='px-5 py-5'>insight 1</Card>
+                //     </div>
+                // </div>
+                // ...existing code...
 
-            <div className="flex flex-col gap-5 justify-center">
+}
+            <div className="flex flex-col md:flex-row mx-auto gap-5 justify-center">
                 <div className="self-center">
                     <ChartSentimen />
                 </div>
